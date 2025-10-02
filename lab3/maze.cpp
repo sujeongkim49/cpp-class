dfe983896b344398c3c69dcb5bbfba2c3249983d
sujeongkim49 <kimsujeong0913@gmail.com>
2025-10-02 10:03:48 +0900

    lab3

---
+++ lab3/maze.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+const int MAXSIZE = 100; // up to 100 by 100 maze allowed
+bool maze[MAXSIZE + 2][MAXSIZE + 2];
+bool mark[MAXSIZE + 1][MAXSIZE + 1] = { 0 };
+enum directions { N, NE, E, SE, S, SW, W, NW };
+struct offsets
+{
+    int a, b;
+} movea[8] = { -1,0, -1,1, 0,1, 1,1, 1,0, 1,-1, 0,-1, -1,-1 };
+struct Items {
+    Items(int xx = 0, int yy = 0, int dd = 0) : x(xx), y(yy), dir(dd) {}
+    int x, y, dir;
+};
+template <class T>
+ostream& operator<< (ostream& os, stack<T>& s) {
+    stack<T> tempStack = s;
+    stack<T> reverseStack;
+    while (!tempStack.empty()) {
+        reverseStack.push(tempStack.top());
+        tempStack.pop();
+    }
+
+    while (!reverseStack.empty()) {
+        os << reverseStack.top();
+        reverseStack.pop();
+        if (!reverseStack.empty()) os << " -> ";
+    }
+    return os;
+}
+ostream& operator<<(ostream& os, Items& item)
+{
+    static int count = 0; // 5개의 Items가 출력될 때마다 줄 바꾸기 위해
+    os << "(" << item.x << "," << item.y << ")";
+    count++; if ((count % 5) == 0) cout << endl;
+    return os;
+}
+void Path(const int m, const int p)
+{
+    mark[1][1] = 1;
+    stack<Items> stack; // C++ STD stack을 이용하기
+    Items temp(1, 1, E);
+    stack.push(temp);
+    while (!stack.empty()) {
+        temp = stack.top(); stack.pop();
+        int i = temp.x;
+        int j = temp.y;
+        int d = temp.dir;
+
+        while (d < 8) {
+            int g = i + movea[d].a;
+            int h = j + movea[d].b;
+            if ((g == m) && (h == p)) {
+                cout << stack;
+                Items item(i, j, d);
+                cout << " -> " << item;
+                item.x = m; item.y = p;
+                cout << " -> " << item << endl;
+                return;
+            }
+            if (!maze[g][h] && !mark[g][h]) {
+                mark[g][h] = 1;
+                temp.x = i; temp.y = j; temp.dir = d + 1;
+                stack.push(temp);
+                i = g; j = h; d = N;
+            }
+            else d++;
+        }
+    }
+    cout << endl;
+}
+void getdata(istream& is, int& m, int& p)
+{ // 자료화일을 읽어들여 maze에 저장한다.
+    is >> m >> p;
+    for (int i = 0; i < m + 2; i++) { maze[i][0] = 1; maze[i][p + 1] = 1; }
+    for (int j = 1; j <= p; j++) { maze[0][j] = 1; maze[m + 1][j] = 1; }
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= p; j++)
+            is >> maze[i][j];
+}
